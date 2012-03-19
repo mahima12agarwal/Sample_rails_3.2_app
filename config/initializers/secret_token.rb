a1dd34b04ba70821ea4766feef86e156cd6827fe
mahima.agarwal <mahima.agarwal@accenture.com>
2012-03-19 18:43:22 +0530

    Initial commit app 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyScrapbook::Application.config.secret_token = '1c68a1cdc278e8409d62b76edb0f948fa2e118254f0bf7197562130008e4077d486fdeee6563bd23556066e28f1fb38db11cd317a291a77eccf7ddef55570dae'
